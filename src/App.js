457a293099650970e4a73d48d087d254364092de
Milo Rastgoo <hello@informagician.com>
2020-01-21 19:47:13 -0500

    steps 4 to 7 completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, createContext } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 import ProductContext from './contexts/ProductContext';
-
+import CartContext from './contexts/CartContext';
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -21,21 +21,25 @@ function App() {
 
 	return (
 		<div className="App">
-			<ProductContext.Provider value={{ products, addItem }}>
-				<Navigation cart={cart} />
-
-				{/* Routes */}
-				<Route
-					exact
-					path="/"
-					component={Products}
-				/>
-
-				<Route
-					path="/cart"
-					render={() => <ShoppingCart cart={cart} />}
-				/>
-			</ProductContext.Provider>
+				<ProductContext.Provider value={{ products, addItem }}>
+					<CartContext.Provider value={cart}>
+						<Navigation/>
+					
+						{/* Routes */}
+					
+						<Route
+							exact
+							path="/"
+							component={Products}
+						/>
+					
+					
+						<Route
+							path="/cart"
+							component={ShoppingCart}
+						/>
+					</CartContext.Provider>
+				</ProductContext.Provider>
 		</div>
 	);
 }
