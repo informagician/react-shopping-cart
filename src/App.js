615115f8a81454da002097484c18f8fdde583931
Milo Rastgoo <hello@informagician.com>
2020-01-21 19:04:50 -0500

    step one - shopping cart is now working

--- src/App.js
+++ src/App.js
@@ -12,7 +12,10 @@ function App() {
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
-		// add the given item to the cart
+		const newCart = [...cart,
+			item
+		];
+		setCart(newCart)
 	};
 
 	return (
